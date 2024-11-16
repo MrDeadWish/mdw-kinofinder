8d888597540068aed973e468f3f710d319082e86
Artem <petrov.art_21@mail.ru>
2024-11-16 10:57:30 +0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-blue-100">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link bg-red-500"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
